533616cc272652354c8df505270ef93e22b96b73
billybu10 <101750525+billybu10@users.noreply.github.com>
2024-03-17 17:56:27 +0100

    Add files via upload

---
+++ include/strings_and_links.hpp
@@ -0,0 +1,9 @@
+//links to bot logo, github and api
+
+#pragma once
+#define BOT_NAME "WeatherBot"
+#define REPO "https://github.com/billybu10/weatherbot"
+#define REPO_README "https://github.com/billybu10/weatherbot#readme"
+#define BOT_IMG "https://raw.githubusercontent.com/billybu10/weatherbot/main/suncloud.png"
+#define API_URL_CURRENT "https://api.weatherapi.com/v1/current.json"
+#define API_URL_FORECAST "https://api.weatherapi.com/v1/forecast.json"
---
+++ include/tokens.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#define BOT_TOKEN "put discord bot token here"
+#define API_TOKEN "put weather api token here"
